a83ce5966c104e272b1c162817beb73f3fd04f06
luis r <luisresendez_@hotmail.com>
2025-02-27 12:45:51 -0600

    first login implementation

---
+++ controllers/login.controllers.js
@@ -0,0 +1,11 @@
+import {sqlConnect , sql} from "../utils/sql.js";
+
+export const login = async (req, res) => {
+    const pool = await sqlConnect();
+    const result = await pool
+        .request()
+        .input("username", sql.VarChar, req.body.username)
+        .query("SELECT * FROM users WHERE username = @username");
+    let isLogin = result.recordset[0].password === req.body.password // result.recordset[0] is the first row of the result which is only one row in this case
+    res.status(200).json({ isLogin : isLogin});
+};
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import indexRoutes from "./routes/index.routes.js"
 import itemsRoutes from "./routes/items.routes.js"
-
+import loginRoutes from "./routes/login.routes.js"
 
 const app = express();
 
@@ -11,6 +11,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(indexRoutes);
 app.use(itemsRoutes);
+app.use(loginRoutes);
 
 
 app.listen(PORT, () => {
