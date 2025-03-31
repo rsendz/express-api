86b8327abafa54a13e4a94e5b16d6b5bca516d45
luis r <luisresendez_@hotmail.com>
2025-03-31 12:03:50 -0600

    fix firebase connection

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,25 +1,11 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import indexRoutes from "./routes/index.routes.js";
-import itemsRoutes from "./routes/items.routes.js";
-import items2Routes from "./routes/items2.routes.js";
-import items3Routes from "./routes/items3.routes.js";
-import loginRoutes from "./routes/login.routes.js";
-import morgan from "morgan";
-import { connectDB } from "./utils/mongodb.js";
+import firebase from "firebase-admin";
+import serviceAccount from "./key.json" with {type:"json"};
+import { getFirestore } from "firebase-admin/firestore";
 
-const app = express();
+const app = firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+});
 
-connectDB();
+const db = getFirestore(app);
 
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(indexRoutes);
-app.use(itemsRoutes);
-app.use(items2Routes);
-app.use(items3Routes);
-app.use(loginRoutes);
-
-app.listen(5000, console.log("http://localhost:5000"));
+export default db;
